918b6cd25b54e940ab2666193238678feeb52a57
Dovydas Luksa <dluksa20@gmail.com>
2024-08-22 02:19:03 +0100

    Second commit

--- ticky_check.py
+++ ticky_check.py
@@ -1,67 +1,48 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+
 import sys
-import re
 import operator
+import re
 import csv
 
-# Dict: Count number of entries for each user
-per_user = {}  # Splitting between INFO and ERROR
-# Dict: Number of different error messages
 errors = {}
+per_user = {}
 
-# * Read file and create dictionaries
-with open('syslog.log') as file:
-    # read each line
-    for line in file.readlines():
-        # regex search
-        # * Sample Line of log file
-        # "May 27 11:45:40 ubuntu.local ticky: INFO: Created ticket [#1234] (username)"
-        match = re.search(
-            r"ticky: ([\w+]*):? ([\w' ]*)[\[[#0-9]*\]?]? ?\((.*)\)$", line)
-        code, error_msg, user = match.group(1), match.group(2), match.group(3)
+pattern = r'ticky: ([A-Z]+) (\w.*) \((\w.*)\)$'
+with open(sys.argv[1], 'r') as file:
+    for log in file.readlines():
+        match = re.search(pattern, log)
+        info, msg, usr = match.group(1), match.group(2), match.group(3)
 
-        # Populates error dict with ERROR messages from log file
-        if error_msg not in errors.keys():
-            errors[error_msg] = 1
-        else:
-            errors[error_msg] += 1
-        # Populates per_user dict with users and default values
-        if user not in per_user.keys():
-            per_user[user] = {}
-            per_user[user]['INFO'] = 0
-            per_user[user]['ERROR'] = 0
-        # Populates per_user dict with users logs entry
-        if code == 'INFO':
-            if user not in per_user.keys():
-                per_user[user] = {}
-                per_user[user]['INFO'] = 0
+        # add users to dictionary if not added yet
+        if usr not in per_user.keys():
+            per_user[usr] = {}
+            per_user[usr]['INFO'] = 0
+            per_user[usr]['ERROR'] = 0
+        # update usr dictionary 
+        if info == 'INFO':
+            per_user[usr]['INFO']+=1
+        elif info == 'ERROR':
+            per_user[usr]['ERROR']+=1
+        # update error dictionary if erro log found 
+        if info == 'ERROR':
+            if msg not in errors.keys():
+                errors[msg] = 1
             else:
-                per_user[user]["INFO"] += 1
-        elif code == 'ERROR':
-            if user not in per_user.keys():
-                per_user[user] = {}
-                per_user[user]['INFO'] = 0
-            else:
-                per_user[user]['ERROR'] += 1
+                errors[msg]+=1
+# errors = sorted(errors.items(), key=operator.itemgetter(1), reverse=True)
+errors_sorted = sorted(errors.items(), key=operator.itemgetter(1), reverse=True)
+per_user_sorted = sorted(per_user.items(), key=operator.itemgetter(0))
 
-
-# Sorted by VALUE (Most common to least common)
-errors_list = sorted(errors.items(), key=operator.itemgetter(1), reverse=True)
-
-# Sorted by USERNAME
-per_user_list = sorted(per_user.items(), key=operator.itemgetter(0))
-
-file.close()
-# Insert at the beginning of the list
-errors_list.insert(0, ('Error', 'Count'))
-
-# * Create CSV file user_statistics
-with open('user_statistics.csv', 'w', newline='') as user_csv:
-    for key, value in per_user_list:
-        user_csv.write(str(key) + ',' +
-                       str(value['INFO']) + ',' + str(value['ERROR'])+'\n')
-
-# * Create CSV error_message
-with open('error_message.csv', 'w', newline='') as error_csv:
-    for key, value in errors_list:
-        error_csv.write(str(key) + ' ' + str(value))
+with open('error.csv', 'w') as error:
+    fieldnames = ['Error', 'Count']
+    csvw = csv.DictWriter(error, fieldnames=fieldnames)
+    csvw.writeheader()
+    for key, value in errors_sorted:
+        csvw.writerow({'Error': key, 'Count': value})
+with open('user.csv', 'w') as error:
+    fieldnames = ['User', 'INFO', 'ERROR']
+    csvw = csv.DictWriter(error, fieldnames=fieldnames)
+    csvw.writeheader()
+    for key, value in per_user_sorted:
+        csvw.writerow({'User': key, 'INFO': value['INFO'], 'ERROR': value['ERROR']})
